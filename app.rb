d123706651618ad49c8598d6974fc374fc209296
Anyan <anyanxie@gmail.com>
2020-03-11 11:12:03 -0400

    added user test (untested)

--- app.rb
+++ app.rb
@@ -13,7 +13,58 @@ require 'sinatra'
 require './models'
 
 get '/' do
-  "Hello Sinatra!"
+  @users = User.all
+  erb :index
+end
+
+get '/api/users/:name' do
+  content_type :json
+  user = User.find_by(name: params[:name])
+  if user.nil?
+    status 404
+    {}.to_json
+  else
+    status 200
+    content = {name:user.name, email:user.email, self_intro:user.self_intro, id:user.id}
+    # return content in json
+    content.to_json
+  end 
+end
+
+post '/api/users' do
+  user = User.create(JSON.parse(request.body.read))
+  status 200
+end
+
+delete '/api/users/:name' do
+  user = User.find_by(name: params[:name])
+  if user.nil?
+    status 404
+  else
+    user.destroy
+    status 200
+  end
+end
+
+post '/api/users/:name/validate' do
+  data = JSON.parse(request.body.read)
+  user = User.find_by(name: params[:name]) 
+  if user.password == data["password"] && user.name == data["name"] && user.self_intro == data["self_intro"] && user.email == data["email"]
+    status 200
+  else
+    status 404
+  end
+end
+
+put "/api/users/:id" do
+  data = JSON.parse(request.body.read)
+  user = User.find(params[:id])
+  if user.nil?
+    status 404
+  else
+    status 200
+    user.update(data)
+  end
 end
 
 # sign up： create a new user
--- test/root_test.rb
+++ test/root_test.rb
@@ -10,11 +10,122 @@ def app
   Sinatra::Application
 end
 
-describe 'The HelloWorld App' do
+describe "GET on /api/users/:id" do
+  before do
+    User.delete_all
+    User.create(
+      name: "paul",
+      email: "paul@gmail.com",
+      password: "foo",
+      self_intro: "Student")
+  end
 
-  it "says hello" do
-    get '/'
+  it "Should return user by name" do
+    get '/api/users/paul'
     last_response.ok?
-    last_response.body.must_equal 'Hello Sinatra!'
+    #puts "#{last_response.body}"
+    attributes = JSON.parse(last_response.body)
+    _(attributes["name"]).must_equal "paul"
+  end
+
+  it "Should return users email too" do
+    get '/api/users/paul'
+    last_response.ok?
+    attributes = JSON.parse(last_response.body)
+    _(attributes["email"]).must_equal "paul@gmail.com"
+  end
+  
+  it "Should not return password" do
+    get '/api/users/paul'
+    last_response.ok?
+    attributes = JSON.parse(last_response.body)
+    _(attributes.key?("password")).must_equal false
   end
 end
+
+describe "POST on /api/users" do
+  it "Should create a user" do
+    post '/api/users', {
+      name: "manzi",
+      email: "manzi@gmail.com",
+      password: "lotus",
+      self_intro: "Lotus CEO"}.to_json
+    last_response.ok?
+    get '/api/users/manzi'
+    last_response.ok?
+    attributes = JSON.parse(last_response.body)
+    _(attributes["email"]).must_equal "manzi@gmail.com"
+    _(attributes["name"]).must_equal "manzi"
+    _(attributes["self_intro"]).must_equal "Lotus CEO"
+  end
+end
+
+describe "DELETE on api/users/:name" do
+  before do
+    User.delete_all
+    User.create(
+      name: "julian",
+      email: "julian@gmail.com",
+      password: "foo",
+      self_intro: "Student")
+  end
+
+  it "Should delete a user" do
+    delete '/api/users/julian'
+    last_response.ok?
+    get '/api/users/julian'
+    last_response.ok?
+    attributes = JSON.parse(last_response.body)
+    _(attributes.key?("name")).must_equal false
+  end
+end
+
+describe "PUT /api/users/:name/validate" do
+  before do
+    User.delete_all
+    User.create(
+      name: "julian",
+      email: "julian@gmail.com",
+      password: "foo",
+      self_intro: "Student")
+  end
+
+  it "Should validate a user" do
+    post '/api/users/julian/validate', {
+      name: "julian",
+      email: "julian@gmail.com",
+      password: "foo",
+      self_intro: "Student"}.to_json
+    _(last_response.ok?).must_equal true
+  end
+end
+
+describe "POST /api/users/:id" do
+  before do
+    User.delete_all
+    User.create(
+      name: "julian",
+      email: "julian@gmail.com",
+      password: "foo",
+      self_intro: "Student")
+  end
+
+  it "Should update a user" do
+    get '/api/users/julian'
+    last_response.ok?
+    attributes = JSON.parse(last_response.body)
+    id = attributes["id"]
+    put '/api/users/'+id.to_s, {
+      name: "julian",
+      email: "anotheremail@gmail.com",
+      password: "foo",
+      self_intro: "Student"}.to_json
+    last_response.ok?
+
+    get '/api/users/julian'
+    last_response.ok?
+    attributes = JSON.parse(last_response.body)
+    _(attributes["email"]).must_equal "anotheremail@gmail.com"
+  end
+end
+
