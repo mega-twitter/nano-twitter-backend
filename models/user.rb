1d6766611f57b37cc63fd0217d16f3609955ea17
BubbleShadow <yifei.han@uconn.edu>
2020-03-10 21:45:28 -0400

    Update user.rb

--- models/user.rb
+++ models/user.rb
@@ -1,22 +1,31 @@
 require 'bcrypt'
 class User < ActiveRecord::Base
-    include BCrypt
-    # attr_accessor :password
+  include BCrypt
+  # attr_accessor :password
     
-    validates_presence_of :name
-    validates_presence_of :email
-    validates_length_of :password, minimum: 3
+  validates_presence_of :name
+  validates_presence_of :email
+  validates_length_of :password, minimum: 3
 
-    def self.authenticate(user_email, password)
-        user = find_by(email: user_email)
-        if user.nil?
-          return false
-        else
-          return user.password == password
-        end
+  def self.authenticate(user_email, password)
+    user = find_by(email: user_email)
+    if user.nil?
+      return {
+        :user_id => nil,
+        :status => "nil user"
+      }.to_json
+    else
+      if user.password == password
+        return {
+          :user_id => user
+          :status => "success"
+        }.to_json
+      else
+        return {
+          :user_id => user
+          :status => "wrong password"
+        }.to_json
       end
-
-
-    
-
+    end
   end
+
