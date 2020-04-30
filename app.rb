7d2ba487589107d378be92b9e2579d1d852166f3
BubbleShadow <yifei.han@uconn.edu>
2020-04-30 18:08:17 -0400

    Update app.rb

--- app.rb
+++ app.rb
@@ -5,13 +5,19 @@ require 'json'
 require_relative './models/tweet'
 require_relative './models/user'
 
-get '/search' do 
+get '/search_tweet' do 
   @key_word = params[:search].downcase
-  sql = "SELECT tweets.* FROM tweets, plainto_tsquery('#{@key_word}') AS q WHERE (tsv_tweet @@ q) 
+  sql = "SELECT user_id, content, created_at FROM tweets, plainto_tsquery('#{@key_word}') AS q WHERE (tsv_tweet @@ q) 
   ORDER BY tweets.created_at DESC"
   @result_tweet = ActiveRecord::Base.connection.execute(sql)
-  # @result_user = User.all.where("lower(name) LIKE ?", "%#{@key_word}%")
   status 200
   @result_tweet.to_json
 end
 
+get '/search_user' do 
+  @key_word = params[:search].downcase
+  sql = "SELECT id, name FROM users where lower(name) LIKE '%#{@key_word}%'"
+  @result_user = ActiveRecord::Base.connection.execute(sql)
+  status 200
+  @result_user.to_json
+end
